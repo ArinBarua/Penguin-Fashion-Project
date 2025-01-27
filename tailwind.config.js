2723f5b5061e87e0ea0826b8a28ec15b279d1da9
Arin Barua <arinbarua1994@gmail.com>
2025-01-27 21:56:25 +0600

    added ladies section

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,13 +2,7 @@
 module.exports = {
     content: ["./src/**/*.{html,js}"],
     theme: {
-        fontFamily: {
-            'bebas-neue-regular': ["Bebas Neue", 'serif'],
-            'roboto-sec': ["Roboto", 'serif'],
-        },
-        extend: {
-            'title-black': '#363958','light-black': '#3E3E3E',
-        },
+        extend: {},
     },
 
     plugins: [],
